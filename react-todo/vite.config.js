5f25e896ae9a6c0df5423ee3db7183413024dda8
ali shabani qomi <alishqomi2486@gmail.com>
2025-08-16 18:18:14 +0330

    Update vite.config.js base path

--- react-todo/vite.config.js
+++ react-todo/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚡️ تنظیمات Vite برای GitHub Pages
 export default defineConfig({
   plugins: [react()],
+  // ❗️ اینجا حتماً اسم ریپازیتوری‌ت رو بذار
+  base: '/todo/',
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
